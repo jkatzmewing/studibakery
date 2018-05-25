5f702acf4e073a28327cafc46196ad484eaf87f6
miramor <daniel.levine013@gmail.com>
2018-05-25 15:48:58 -0400

    Use Docker CE
    "docker" package in Debian is actually a GTK desktop app

--- docker-setup-debian.sh
+++ docker-setup-debian.sh
@@ -2,9 +2,28 @@
 
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo apt-get install \
-	bash-completion \
-	cron-apt \
-	docker \
-	git
 
+# Prereqs
+sudo apt-get install \
+	apt-transport-https \
+	bash-completion \
+	ca-certificates \
+	cron-apt \
+	git \
+	gnupg2 \
+	software-properties-common
+
+# Docker repo key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+# Verify to the user that the key installed correctly
+# See the docs at:
+# https://docs.docker.com/install/linux/docker-ce/debian/#set-up-the-repository
+sudo apt-key fingerprint 0EBFCD88
+# Add and enable the repo
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	$(lsb_release -cs) \
+	stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
