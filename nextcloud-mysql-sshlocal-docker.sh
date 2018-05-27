aa162463512bba289fccb17f77fc776639e6198c
miramor <daniel.levine013@gmail.com>
2018-05-27 17:04:41 -0400

    Create nextcloud MySQL database

--- nextcloud-mysql-sshlocal-docker.sh
+++ nextcloud-mysql-sshlocal-docker.sh
@@ -18,6 +18,7 @@ sudo docker network create net-nextcloud-mysql
 sudo docker run -d \
 	-e MYSQL_ROOT_PASSWORD="$mysql_rootpw" \
 	-e MYSQL_USER=nextcloud \
+	-e MYSQL_DATABASE=nextcloud \
 	-e MYSQL_PASSWORD="$mysql_userpw" \
 	--name mysql \
 	--network net-nextcloud-mysql \
