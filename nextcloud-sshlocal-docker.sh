d07c69e85ad87dcafa50d18fb8031aeb83c8743c
miramor <daniel.levine013@gmail.com>
2018-05-27 16:31:43 -0400

    Add script for local Nextcloud

---
+++ nextcloud-sshlocal-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Set up local Nextcloud (with SQLite) for use over SSH tunnels
+
+sudo docker pull nextcloud
+sudo mkdir -p /mnt/data/nextcloud
+sudo docker run -d \
+	-v /mnt/data/nextcloud:/var/www/html \
+	--name nextcloud \
+	-p 8080:3080 \
+	nextcloud
