af574a38dada4b89f283e80363808853bd21248b
miramor <daniel.levine013@gmail.com>
2018-06-23 14:27:23 -0400

    Setup script for Apache + basic site

---
+++ host/apache2/apache2-setup-debian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install apache2
+
+sudo a2dissite default default-ssl
+sudo a2enmod ssl
+
+resource_dir="host/apache2/resources"
+sudo cp "$resource_dir"/index.html /var/www/html/
+sudo cp "$resource_dir"/hottestgirlintheclub.conf /etc/apache2/sites-available
+sudo systemctl reload apache2
