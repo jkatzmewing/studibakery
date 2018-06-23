484928f968a052e2caabf17b56c6e0e8fe626638
miramor <daniel.levine013@gmail.com>
2018-06-23 14:30:00 -0400

    Correct default HTTP site config name

--- host/apache2/apache2-setup-debian.sh
+++ host/apache2/apache2-setup-debian.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install apache2
 
-sudo a2dissite default default-ssl
+sudo a2dissite 000-default default-ssl
 sudo a2enmod ssl
 
 resource_dir="host/apache2/resources"
