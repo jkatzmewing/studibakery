8bf090cbe8cd0ee2ea723f928243834e77327943
miramor <beeswaxboffin@gmail.com>
2018-09-12 19:09:40 -0400

    Copy all HTML and CSS files, not just the index

--- host/apache2/apache2-setup-debian.sh
+++ host/apache2/apache2-setup-debian.sh
@@ -7,6 +7,6 @@ sudo a2dissite 000-default default-ssl
 sudo a2enmod ssl
 
 resource_dir="host/apache2/resources"
-sudo cp "$resource_dir"/index.html /var/www/html/
+sudo cp "$resource_dir"/*.{css,html} /var/www/html/
 sudo cp "$resource_dir"/hottestgirlintheclub.conf /etc/apache2/sites-available
 sudo systemctl reload apache2
