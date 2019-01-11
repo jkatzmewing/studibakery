1c1d54fb67805ef5003f203c52e236d1835bbb8f
miramor <beeswaxboffin@gmail.com>
2019-01-11 09:42:00 -0500

    Update Apache setup for Nextcloud

--- host/apache2/apache2-setup-debian.sh
+++ host/apache2/apache2-setup-debian.sh
@@ -4,9 +4,8 @@ sudo apt-get update
 sudo apt-get install apache2
 
 sudo a2dissite 000-default default-ssl
-sudo a2enmod ssl
+sudo a2enmod ssl proxy proxy_http
 
 resource_dir="host/apache2/resources"
-sudo cp "$resource_dir"/*.{css,html} /var/www/html/
 sudo cp "$resource_dir"/hottestgirlintheclub.conf /etc/apache2/sites-available
 sudo systemctl reload apache2
