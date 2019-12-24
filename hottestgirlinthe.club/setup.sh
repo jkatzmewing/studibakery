e00593d62d9425b695067315a8ae85d44f184d79
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2019-12-24 15:07:43 -0500

    Standardize script names and hierarchy

--- hottestgirlinthe.club/setup.sh
+++ hottestgirlinthe.club/setup.sh
@@ -2,8 +2,8 @@
 
 ./common/packages.sh
 
-./host/docker-setup-debian.sh
-./host/apache/apache2-setup-debian.sh "hottestgirlinthe.club"
-./host/certbot/cert-setup.sh "hottestgirlinthe.club"
+./host/docker/setup.sh
+./host/apache/setup.sh "hottestgirlinthe.club"
+./host/certbot/setup.sh "hottestgirlinthe.club"
 
-./nextcloud/nextcloud-mysql-docker-setup.sh "17" "/mnt/data"
+./nextcloud/setup.sh "17" "/mnt/data"
