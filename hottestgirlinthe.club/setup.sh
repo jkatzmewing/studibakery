f1a4159f02f6bc9bb28923875334dcf593154d06
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2019-12-24 15:21:31 -0500

    Automatically set the correct hostname in Nextcloud

--- hottestgirlinthe.club/setup.sh
+++ hottestgirlinthe.club/setup.sh
@@ -7,3 +7,7 @@
 ./host/certbot/setup.sh "hottestgirlinthe.club"
 
 ./nextcloud/setup.sh "17" "/mnt/data"
+
+# Set correct hostname in Nextcloud
+sudo docker exec --user www-data nextcloud php occ config:system:set overwritehost --value="hottestgirlinthe.club:443"
+sudo docker stop nextcloud && sudo docker start nextcloud
