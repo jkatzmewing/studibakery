edd24789c55cde1e75c0f92cf50b1ea6d0a03ef8
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-01-20 18:26:26 -0500

    Enable cache_disk module as well as cache

--- host/apache2/setup.sh
+++ host/apache2/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install apache2
 
 sudo a2dissite 000-default default-ssl
-sudo a2enmod ssl proxy proxy_http headers http2 cache
+sudo a2enmod ssl proxy proxy_http headers http2 cache cache_disk
 
 resource_dir="host/apache2/resources"
 sudo cp "$resource_dir"/"$my_domain".conf /etc/apache2/sites-available
