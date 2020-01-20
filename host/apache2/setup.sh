e0335070b7196ea3ecd1868782f6b508889371c8
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-01-20 17:38:11 -0500

    Enable httpd disk cache, and restart httpd instead of reloading

--- host/apache2/setup.sh
+++ host/apache2/setup.sh
@@ -6,9 +6,9 @@ sudo apt-get update
 sudo apt-get install apache2
 
 sudo a2dissite 000-default default-ssl
-sudo a2enmod ssl proxy proxy_http headers http2
+sudo a2enmod ssl proxy proxy_http headers http2 cache
 
 resource_dir="host/apache2/resources"
 sudo cp "$resource_dir"/"$my_domain".conf /etc/apache2/sites-available
 sudo a2ensite "$my_domain"
-sudo systemctl reload apache2
+sudo systemctl restart apache2
