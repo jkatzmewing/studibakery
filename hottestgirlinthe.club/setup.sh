2c90e7fa88c35888b427afb9e4828c7e77995d1c
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-02-11 14:19:20 -0500

    Add gitea containers to upgrade and setup scripts

--- hottestgirlinthe.club/setup.sh
+++ hottestgirlinthe.club/setup.sh
@@ -7,6 +7,7 @@
 ./host/certbot/setup.sh "hottestgirlinthe.club"
 
 ./nextcloud/setup.sh "18" "/mnt/data"
+./gitea/setup.sh "1.11" "/mnt/data"
 
 # Set correct hostname in Nextcloud
 sudo docker exec --user www-data nextcloud php occ config:system:set overwritehost --value="hottestgirlinthe.club:443"
--- hottestgirlinthe.club/upgrade.sh
+++ hottestgirlinthe.club/upgrade.sh
@@ -5,3 +5,5 @@ sudo apt-get update && sudo apt-get -qqy dist-upgrade
 ./bin/occ.sh maintenance:mode --on
 ./nextcloud/upgrade.sh "18" "/mnt/data"
 ./bin/occ.sh maintenance:mode --off
+
+./gitea/upgrade.sh "1.11" "/mnt/data"
