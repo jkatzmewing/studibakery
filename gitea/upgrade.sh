47a0a94b5c7da7fa690f39259f048ea1dba6bc8e
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-02-11 14:12:13 -0500

    Add setup files for gitea

---
+++ gitea/upgrade.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Upgrade gitea containers
+
+gitea_version="$1"
+gitea_data_dir="$2/gitea"
+gitea_db_dir="$2/gitea_mysql"
+
+sudo docker pull mariadb:latest
+sudo docker pull gitea/gitea:"$gitea_version"
+
+sudo docker stop web
+sudo docker rm web
+sudo docker stop db
+sudo docker rm db
+
+sudo docker run -d \
+	--name db \
+	--network net-gitea \
+	-v "$gitea_db_dir":/var/lib/mysql \
+	mariadb:latest
+
+sudo docker run -d \
+	--name web \
+	--network net-gitea \
+	-v "$gitea_data_dir":/data \
+	-p 3000:3000 \
+	-p 2222:22 \
+	gitea/gitea:"$gitea_version"
