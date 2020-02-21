eb4f74be88e55a84ad44f461ae8763e97effa958
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-02-20 19:12:38 -0500

    Add a 3 second sleep before trying to turn off Nextcloud maintenance mode

--- hottestgirlinthe.club/upgrade.sh
+++ hottestgirlinthe.club/upgrade.sh
@@ -4,6 +4,7 @@ sudo apt-get update && sudo apt-get -qqy dist-upgrade
 
 ./bin/occ.sh maintenance:mode --on
 ./nextcloud/upgrade.sh "18" "/mnt/data"
+sleep 3 # give things time to settle so turning off maintenance mode succeeds
 ./bin/occ.sh maintenance:mode --off
 
 ./gitea/upgrade.sh "1.11" "/mnt/data"
