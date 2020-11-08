251fd15cc4763d271a5c0fc00b9de24a250fa942
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-11-08 13:05:34 -0500

    Tweak certbot settings a bit so automation actually works

--- host/certbot/setup.sh
+++ host/certbot/setup.sh
@@ -3,5 +3,5 @@
 my_domain="$1"
 
 sudo apt-get update && sudo apt-get -qqy install certbot python-certbot-apache
-sudo certbot certonly --apache -d "$my_domain"
+sudo certbot certonly --standalone -d "$my_domain"
 
--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@
 ./common/packages.sh
 
 ./host/docker/setup.sh
-./host/apache2/setup.sh "hottestgirlinthe.club"
-./host/apache2/setup.sh "janalexandrakatz.com"
 
 ./host/certbot/setup.sh "hottestgirlinthe.club"
 ./host/certbot/setup.sh "janalexandrakatz.com"
+
+./host/apache2/setup.sh "hottestgirlinthe.club"
+./host/apache2/setup.sh "janalexandrakatz.com"
 
 ./nextcloud/setup.sh "20" "/mnt/data"
 ./gitea/setup.sh "1.12" "/mnt/data"
