71c375248a16100de36b7bf5f585a30bedae1bdb
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-12-21 16:21:44 -0500

    Upgrade to Gitea 1.13

--- upgrade.sh
+++ upgrade.sh
@@ -7,4 +7,4 @@ sudo apt-get update && sudo apt-get -qqy dist-upgrade
 sleep 3 # give things time to settle so turning off maintenance mode succeeds
 ./bin/occ.sh maintenance:mode --off
 
-./gitea/upgrade.sh "1.12" "/mnt/data"
+./gitea/upgrade.sh "1.13" "/mnt/data"
