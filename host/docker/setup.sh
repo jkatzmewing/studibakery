9eca77e23958b3e68fd009b06574c5162228da4f
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2021-08-15 14:00:00 -0400

    Clean up extremely broken docker setup script (IDK where my brain was at the time)

--- host/docker/setup.sh
+++ host/docker/setup.sh
@@ -10,15 +10,12 @@ else # Assume we're on Debian
 	# Verify to the user that the key installed correctly
 	# See the docs at:
 	# https://docs.docker.com/install/linux/docker-ce/debian/#set-up-the-repository
-	if [ "$distro" = "debian" ]; then
-		sudo apt-key fingerprint 0EBFCD88
-		# Add and enable the repo
-		sudo add-apt-repository \
-			"deb [arch=amd64] https://download.docker.com/linux/debian \
-			$(lsb_release -cs) \
-			stable"
-	elif [ "$distro" = "ubuntu" ]; then
-
+	sudo apt-key fingerprint 0EBFCD88
+	# Add and enable the repo
+	sudo add-apt-repository \
+		"deb [arch=amd64] https://download.docker.com/linux/debian \
+		$(lsb_release -cs) \
+		stable"
 
 	sudo apt-get update
 	sudo apt-get install -y docker-ce
